389966b8599b1cb71d8dc8adfe3a861167732975
Lehi Galabo <galabslehi75@gmail.com>
2026-02-16 02:01:35 +0800

    adjusted some css

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+function Indexfunction() {
+  const button = document.querySelector('.create');
+
+  button.addEventListener( 'click', () => {
+    console.log(`Hi Lehi!`);
+  })
+}
+
+Indexfunction();
